fede0635194c146077df9d1fc95eac5b23f23850
Lakshay1303 <161626901+Lakshay1303@users.noreply.github.com>
2024-03-20 22:18:52 +0530

    Create SectionFApplication.java

---
+++ SectionFApplication.java
@@ -0,0 +1,13 @@
+package com.java.testspring.SectionF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectionFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectionFApplication.class, args);
+	}
+
+}
